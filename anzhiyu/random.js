ff1289a50eeac87c29bed2b8fbf1f909e7fab85c
iuuu0oo <hexo-s@outlook.com>
2026-03-29 10:45:42 +0800

    Site updated: 2026-03-29 10:45:38

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/16/hello-world/","2026/03/20/字体鉴赏/"];function toRandomPost(){
+var posts=["2026/03/29/123/","2026/03/29/456/","2026/03/16/hello-world/","2026/03/20/字体鉴赏/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
